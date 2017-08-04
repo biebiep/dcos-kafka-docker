cd7cdec2374847bade501caf0578ae2c0cd9e977
biebiep <bram.steurtewagen@gmail.com>
2017-08-04 15:34:59 +0200

    Update kafka-marathon-bootstrap.sh

--- kafka-marathon-bootstrap.sh
+++ kafka-marathon-bootstrap.sh
@@ -5,11 +5,7 @@
 
 IFS=. read ip1 ip2 ip3 ip4 <<< "$HOST"
 
-# Create unique broker.id as combination of last octet and the given Marathon Docker port
-#broker_id="${ip4}${PORT0}"
-
 # Write custom-server.properties config file
-#echo "broker.id=${broker_id}" > $KAFKA_HOME/config/custom-server.properties
 echo "host.name=${HOST}" >> $KAFKA_HOME/config/custom-server.properties
 echo "port=${PORT0}" >> $KAFKA_HOME/config/custom-server.properties
 echo "log.dir=/var/log/kafka/kafka-${broker_id}" >> $KAFKA_HOME/config/custom-server.properties
